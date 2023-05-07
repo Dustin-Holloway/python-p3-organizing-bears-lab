a28f2593c77b8be7815354f2b1512222b9b2e807
Dustin <dustinhollowaypdx@gmail.com>
2023-05-07 16:11:31 -0700

    added bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 25, "M", "Green", "Mellow", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Brown", "Wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 15, "W", "Blonde", "Mellow", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 55, "M", "Dark Brown", "Tough", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 29, "W", "Pink", "Silly", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 67, "M", "Green", "Crazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", NULL, "W", "Maroon", "Fun", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "M", "Brown", "Mellow", 0);
